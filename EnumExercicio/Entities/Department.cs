a631164072d4a40b87f24e5dc7f20b9ae1fe8b75
Miguel Cortez <miguelcortezrj@gmail.com>
2023-01-30 18:28:22 -0300

    Inicio do desenvolvimento do programa

---
+++ EnumExercicio/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExercicio.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+            Name = "";
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
